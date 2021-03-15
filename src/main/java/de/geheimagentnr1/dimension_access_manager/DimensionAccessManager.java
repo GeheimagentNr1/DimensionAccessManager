4c5e1e755eb2da6248277b55d150793b842e26dc
GeheimagentNr1 <geheimelache@hotmail.com>
2021-03-15 20:54:04 +0100

    Fixed incompatible warning

--- src/main/java/de/geheimagentnr1/dimension_access_manager/DimensionAccessManager.java
+++ src/main/java/de/geheimagentnr1/dimension_access_manager/DimensionAccessManager.java
@@ -1,9 +1,12 @@
 package de.geheimagentnr1.dimension_access_manager;
 
 import de.geheimagentnr1.dimension_access_manager.config.ServerConfig;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 
 @SuppressWarnings( "UtilityClassWithPublicConstructor" )
@@ -16,5 +19,12 @@ public class DimensionAccessManager {
 	public DimensionAccessManager() {
 		
 		ModLoadingContext.get().registerConfig( ModConfig.Type.SERVER, ServerConfig.CONFIG );
+		ModLoadingContext.get().registerExtensionPoint(
+			ExtensionPoint.DISPLAYTEST,
+			() -> Pair.of(
+				() -> FMLNetworkConstants.IGNORESERVERONLY,
+				( remote, isServer ) -> true
+			)
+		);
 	}
 }
