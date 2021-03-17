d292e38231eb7680268b27315665ac13d3b603e1
GeheimagentNr1 <geheimelache@hotmail.com>
2021-03-17 16:03:45 +0100

    buildscript updated licenseReport updated

--- src/main/java/de/geheimagentnr1/dimension_access_manager/util/ResourceLocationHelper.java
+++ src/main/java/de/geheimagentnr1/dimension_access_manager/util/ResourceLocationHelper.java
@@ -17,6 +17,6 @@ public class ResourceLocationHelper {
 	
 	public static String serverWorldToName( ServerWorld serverWorld ) {
 		
-		return Objects.requireNonNull( serverWorld.func_234923_W_().getRegistryName() ).toString();
+		return Objects.requireNonNull( serverWorld.getDimensionKey().getRegistryName() ).toString();
 	}
 }
