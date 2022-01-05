ec34de5956f4f33cd390b8db6443bc083bafa553
GeheimagentNr1 <geheimelache@hotmail.com>
2022-01-05 20:48:46 +0100

    Updated Forge Fixed Dimensions Command has the wrong required

--- src/main/java/de/geheimagentnr1/dimension_access_manager/elements/commands/DimensionsCommand.java
+++ src/main/java/de/geheimagentnr1/dimension_access_manager/elements/commands/DimensionsCommand.java
@@ -22,14 +22,15 @@ public class DimensionsCommand {
 		LiteralArgumentBuilder<CommandSourceStack> dimensions = Commands.literal( "dimensions" );
 		dimensions.then( Commands.literal( "status" )
 			.executes( DimensionsCommand::showDimensionsStatus ) );
-		LiteralArgumentBuilder<CommandSourceStack> manageDimensions = dimensions
+		LiteralArgumentBuilder<CommandSourceStack> opManageDimensions = Commands.literal( "dimensions" )
 			.requires( source -> source.hasPermission( 3 ) );
-		manageDimensions.then( Commands.literal( "default" )
+		opManageDimensions.then( Commands.literal( "default" )
 			.then( Commands.literal( "defaultDimensionAccessType" )
 				.executes( DimensionsCommand::showDefaultDimensionAccessType )
 				.then( Commands.argument( "dimensionAccessType", DimensionAccessTypeArgument.dimensionAccessType() )
 					.executes( DimensionsCommand::setDefaultDimensionAccessType ) ) ) );
 		dispatcher.register( dimensions );
+		dispatcher.register( opManageDimensions );
 	}
 	
 	private static int showDimensionsStatus( CommandContext<CommandSourceStack> context ) {
